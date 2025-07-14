d564c86386002bbe898a96d7e41e6f8ae5aa25fb
jlchacon <jlchacon@xetid.cu>
2025-07-14 09:46:19 -0400

    condicionales en go

---
+++ condicionales/condicionales.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+
+func main() {
+
+	//condicionales en go
+	var edad int = 18
+	if edad >= 18 {
+		fmt.Println("Eres mayor de edad")
+	} else {
+		fmt.Println("Eres menor de edad")
+	}
+}
